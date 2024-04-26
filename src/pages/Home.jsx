9dce6cf0bd17665ae866ee145850eb060211f2a6
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2024-04-26 22:32:27 +0500

    add comit first

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Block from '../components/Block'
+import LoginModal from '../components/LoginModal'
+import RegisterModal from '../components/RegisterModal'
 
 const Home = () => {
   return (
     <div>
         <Hero />
+        <Block />
+        <hr />
+            <LoginModal />
+            <RegisterModal />
+        <hr />
         <Block />
     </div>
   )
